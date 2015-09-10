aa03ac1a9d556c5c3c86deae83c86e6385375d34
arctg <i.duran@yandex.ru>
2015-09-10 18:39:55 +0300

    some refactoring

---
+++ src/ThreadPool/Task.java
@@ -0,0 +1,33 @@
+package ThreadPool;
+
+/**
+ * Created by dennis on 9/10/2015.
+ */
+public class Task implements Runnable {
+
+    private String command;
+
+    public Task(String command) {
+        this.command = command;
+
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " starts. Command is " + command);
+        processCommand();
+        System.out.println(Thread.currentThread().getName() + " ends");
+    }
+
+    public void processCommand() {
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
+    }
+
+    public String toString() {
+        return this.command;
+    }
+}
